42b1dfb1ec468399f460c05e33ad9c09d6a45299
Gavin M. Bell <gavin@6thcolumn.org>
2017-07-17 06:46:52 +0200

    Keeping things tidy

---
+++ personal/custom.el
@@ -0,0 +1,45 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
+ '(custom-safe-themes
+   (quote
+    ("1b27e3b3fce73b72725f3f7f040fd03081b576b1ce8bbdfcb0212920aec190ad" "cdbd0a803de328a4986659d799659939d13ec01da1f482d838b68038c1bb35e8" "ff3ca9d675ad57854fb5485831a3c7d5f34aa47af4de0c24335b7efa02dbb8f9" "82b67c7e21c3b12be7b569af7c84ec0fb2d62105629a173e2479e1053cff94bd" "ce557950466bf42096853c6dac6875b9ae9c782b8665f62478980cc5e3b6028d" "ac5584b12254623419499c3a7a5388031a29be85a15fdef9b94df2292d3e2cbb" "6c0a087a4f49c04d4002393ffd149672f70e4ab38d69bbe8b39059b61682b61c" "fad38808e844f1423c68a1888db75adf6586390f5295a03823fa1f4959046f81" "708df3cbb25425ccbf077a6e6f014dc3588faba968c90b74097d11177b711ad1" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "cc0dbb53a10215b696d391a90de635ba1699072745bf653b53774706999208e3" "b9183de9666c3a16a7ffa7faaa8e9941b8d0ab50f9aaba1ca49f2f3aec7e3be9" "9d7e517b49068e9fef941fe4083ad3d2a4b040895dca5175b84be48739689707" "e6b8d70dd247727d95c92917311e3130e15a21103516a9ce26712ebeb1d95d34" "0820d191ae80dcadc1802b3499f84c07a09803f2cb90b343678bdb03d225b26b" "47ac4658d9e085ace37e7d967ea1c7d5f3dfeb2f720e5dec420034118ba84e17" "443b6e7d3c4636744618884b4872a6f39f02ad819c8811a30f92daf820dd9402" "3038a172e5b633d0b1ee284e6520a73035d0cb52f28b1708e22b394577ad2df1" "e688cf46fd8d8fcb4e7ad683045fbf314716f184779f3f087ef226a4e170837a" "472af27930b4c25b13971cc34ee1610c562366d63ad570d4f7cd0055c182c7e9" "3ff96689086ebc06f5f813a804f7114195b7c703ed2f19b51e10026723711e33" "0c387e27a3dd040b33c6711ff92e13bd952369a788eee97e4e4ea2335ac5528f" "90edd91338ebfdfcd52ecd4025f1c7f731aced4c9c49ed28cfbebb3a3654840b" "0c311fb22e6197daba9123f43da98f273d2bfaeeaeb653007ad1ee77f0003037" "935cc557b01242fc7b4d3f803902d14d1b3afae5123624a2f924255f641f7f01" "520377c32d772b19690013276eb32942593fd194ea257dc28ed5c053ff51e298" "219fa8a4c6a53918f31e7fe94634c115c4f0fb08e45cee6a49b341f810ffef85" "efb148b9a120f417464713fe6cad47eb708dc45c7f2dbfeea4a7ec329214e63e" "108b3724e0d684027c713703f663358779cc6544075bc8fd16ae71470497304f" "bede70e4b2654751936d634040347bb4704fa956ecf7dceab03661a75e46a8ca" "1c50040ec3b3480b1fec3a0e912cac1eb011c27dd16d087d61e72054685de345" "e80932ca56b0f109f8545576531d3fc79487ca35a9a9693b62bf30d6d08c9aaf" "fc0c179ce77997ecb6a7833310587131f319006ef2f630c5a1fec1a9307bff45" default)))
+ '(fci-rule-color "#383838")
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(package-selected-packages
+   (quote
+    (cider scala-mode 0blayout spinner with-editor yasnippet intellij-theme idea-darkula-theme darkroom mustang-theme blackboard-theme bliss-theme borland-blue-theme clues-theme colonoscopy-theme abyss-theme airline-themes ample-theme ample-zen-theme arjen-grey-theme atom-dark-theme autumn-light-theme badger-theme badwolf-theme kubernetes afternoon-theme zop-to-char zenburn-theme yaml-mode which-key volatile-highlights vkill undo-tree soundcloud solarized-theme smex smartrep smartparens smart-mode-line slamhound scala-mode2 restclient rainbow-mode rainbow-delimiters paredit ov operate-on-number noctilux-theme nhexl-mode multiple-cursors move-text minimal-theme midje-mode mellow-theme mbo70s-theme material-theme markdown-mode magit lush-theme lavender-theme kibit-helper json-rpc json-mode js2-mode jazz-theme ir-black-theme inkpot-theme inflections imenu-anywhere ido-ubiquitous hydra htmlize hipster-theme heroku-theme helm-themes helm-projectile helm-descbinds helm-ag hc-zenburn-theme haskell-mode guru-mode gruber-darker-theme grizzl grandshell-theme gotham-theme gotest god-mode go-projectile gitignore-mode gitconfig-mode git-timemachine git-gutter-fringe git-gutter-fringe+ gist ggtags geiser flymd flymake-shell flycheck-protobuf flycheck-color-mode-line flycheck-clojure flx-ido expand-region exec-path-from-shell ensime emamux elisp-slime-nav edn easy-kill dockerfile-mode discover-my-major diminish diff-hl darkburn-theme darcula-theme dakrone-theme d-mode cyberpunk-theme crux company-go company-auctex company-anaconda chef-mode cdlatex bubbleberry-theme browse-kill-ring beacon bash-completion anzu anti-zenburn-theme ant ag ace-window ace-jump-mode ace-jump-buffer ac-octave ac-math ac-ispell ac-html ac-cider)))
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
